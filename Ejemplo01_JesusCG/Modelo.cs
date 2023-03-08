c0fc6ca18271b6b998fe2632ce116c9db77d3092
jcastellanog <jesus.castellano@hotmail.com>
2023-03-08 10:46:39 +0100

    Modificación del Modelo de Datos

---
+++ Ejemplo01_JesusCG/Modelo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejemplo01_JesusCG
+{
+    internal class Modelo
+    {
+        private string _nombre;
+        private string _apellidos;
+    }
+}
